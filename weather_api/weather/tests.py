e6b90c243c258358ad47b1283cf93d9c277ed307
Rapolubharghav <brapolu2@asu.edu>
2024-07-01 00:02:02 -0500

    Updated test cases

--- weather_api/weather/tests.py
+++ weather_api/weather/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from models import WeatherRecord, WeatherStats
+from .models import WeatherRecord, WeatherStats
 
 class WeatherRecordTestCase(TestCase):
     def setUp(self):
@@ -7,9 +7,13 @@ class WeatherRecordTestCase(TestCase):
         WeatherRecord.objects.create(station_id="002", date="2024-06-28", max_temp=32, min_temp=22, precipitation=7)
 
     def test_weather_record(self):
-        record1 = WeatherRecord.objects.get(station_id="001")
-        record2 = WeatherRecord.objects.get(station_id="002")
+        record1 = WeatherRecord.objects.get(station_id="001", date="2024-06-28")
+        record2 = WeatherRecord.objects.get(station_id="002", date="2024-06-28")
         self.assertEqual(record1.max_temp, 30)
+        self.assertEqual(record1.min_temp, 20)
+        self.assertEqual(record1.precipitation, 5)
+        self.assertEqual(record2.max_temp, 32)
+        self.assertEqual(record2.min_temp, 22)
         self.assertEqual(record2.precipitation, 7)
 
 class WeatherStatsTestCase(TestCase):
@@ -17,6 +21,7 @@ class WeatherStatsTestCase(TestCase):
         WeatherStats.objects.create(station_id="001", year=2024, avg_max_temp=30, avg_min_temp=20, total_precipitation=5)
 
     def test_weather_stats(self):
-        stats = WeatherStats.objects.get(station_id="001")
-        self.assertEqual(stats.year, 2024)
+        stats = WeatherStats.objects.get(station_id="001", year=2024)
         self.assertEqual(stats.avg_max_temp, 30)
+        self.assertEqual(stats.avg_min_temp, 20)
+        self.assertEqual(stats.total_precipitation, 5)
